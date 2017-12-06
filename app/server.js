9b4837f757750c826e46a9322b3aec61032a8a04
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-12-06 10:27:11 -0800

    Added config files and wrote server.js for hapi@17

--- app/server.js
+++ app/server.js
@@ -1,3 +1,38 @@
-const hapi = require('hapi');
+const Hapi = require('hapi');
+const config = require('config');
+const pkg = require('./package');
 
-const server = hapi.createServer();
+const { host, port } = config.get('hapiServer');
+
+const server = Hapi.server({ host, port });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => 'Hello, world, from ToDone-server!\n'
+});
+
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, h) => `Hello, ${request.params.name} from ToDone-server!\n`
+});
+
+server.route({
+  method: 'GET',
+  path: '/healthcheck',
+  handler: (request, h) => ({ status: 'ok', service: 'ToDone-server', version: pkg.version })
+});
+
+async function start() {
+  try {
+    await server.start();
+  }
+  catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log(`Server running at ${server.info.uri}`);
+}
+
+start();
