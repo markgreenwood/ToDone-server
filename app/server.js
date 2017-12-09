72237fcc8992c743b04568d4bdf8ab5c1d228742
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-12-08 16:31:57 -0800

    Pulled routes into APIs, testing some 3rd party plugins w/ v17, not working that well...

--- app/server.js
+++ app/server.js
@@ -1,9 +1,10 @@
 const Hapi = require('hapi');
 const config = require('config');
-const pkg = require('./package');
 const es = require('elasticsearch');
 const eb = require('elastic-builder');
 const R = require('ramda');
+
+const pkg = require('./package');
 
 const esClient = new es.Client({ host: config.get('elasticsearch').host });
 
@@ -11,16 +12,37 @@ const { host, port } = config.get('hapiServer');
 
 const server = Hapi.server({ host, port });
 
+const standardApi = {
+  name: 'standardApi',
+  version: '1.0.0',
+  register: async (server, options) => {
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (request, h) => 'Hello, world, from ToDone-server!\n'
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/healthcheck',
+      handler: (request, h) => ({ status: 'ok', service: 'ToDone-server', version: pkg.version })
+    });
+  }
+}
+
 const tasksApi = {
   name: 'tasksApi',
   version: '1.0.0',
-  register: (server, options) => {
+  register: async (server, options) => {
     server.route({
       method: 'GET',
       path: '/tasks',
       handler: (request, h) =>
         esClient.search({ index: 'todone-tasks', type: 'task' })
-          .then(resp => R.map(item => item._source, resp.hits.hits))
+          .then(resp => R.map(item => R.merge(item._source, { itemId: item._id }), resp.hits.hits)),
+      config: {
+        tags: ['api']
+      }
     });
 
     server.route({
@@ -28,49 +50,38 @@ const tasksApi = {
       path: '/tasks/{id}',
       handler: (request, h) =>
         esClient.get({ index: 'todone-tasks', type: 'task', id: request.params.id })
-          .then(resp => resp._source)
+          .then(resp => resp._source),
+      config: {
+        tags: ['api']
+      }
     });
   }
 };
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, h) => 'Hello, world, from ToDone-server!\n'
-});
-
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: (request, h) => `Hello, ${request.params.name} from ToDone-server!\n`
-});
-
-server.route({
-  method: 'GET',
-  path: '/healthcheck',
-  handler: (request, h) => ({ status: 'ok', service: 'ToDone-server', version: pkg.version })
-});
-
-async function registerPlugins() {
+async function registerPlugins(theServer) {
   try {
-    await server.register({ plugin: tasksApi });
+    await theServer.register(standardApi);
+    await theServer.register(tasksApi);
   }
   catch (err) {
     console.log(err);
     process.exit(1);
   }
-  console.log('Plugins registered\n');
+  console.log(`Plugins registered on ${theServer.info.uri}`);
+  return theServer;
 }
 
-async function start() {
+async function startServer(theServer) {
   try {
-    await server.start();
+    await theServer.start();
   }
   catch (err) {
     console.log(err);
     process.exit(1);
   }
-  console.log(`Server running at ${server.info.uri}`);
+  console.log(`Server running at ${theServer.info.uri}`);
+  return theServer;
 }
 
-registerPlugins().then(() => start());
+registerPlugins(server)
+  .then(startServer);
